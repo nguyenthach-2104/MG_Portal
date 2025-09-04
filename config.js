df88ecea7e54736f2b660ef4706fce6630f4ec1d
nguyenthach-2104 <mkt.mgbinhduong@gmail.com>
2025-09-04 21:55:39 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -6,6 +6,6 @@
  * URL của Google Apps Script CHỈ dành cho việc xử lý các yêu cầu
  * liên quan đến xác thực người dùng như đăng nhập, quên mật khẩu, v.v.
  */
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw4zP-qroaAqCpLDS0nLKY4WxgaHZX2o6TURZ5FJh2IPRbVL8dQ3nsQ1MTukgIQjt7W/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyt_qsGTqFojxWZB12Ut0O3RKMwxOpx_zIVg-aOB6AjIFaB6plwaxQ1dYtHveNGLjvxLQ/exec';
 
 // (Không có dấu } ở cuối)
