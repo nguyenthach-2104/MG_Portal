93b2c0edab5571f6ce3a218f03d1802699951551
nguyenthach-2104 <mkt.mgbinhduong@gmail.com>
2025-09-04 21:39:30 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -7,3 +7,5 @@
  * liên quan đến xác thực người dùng như đăng nhập, quên mật khẩu, v.v.
  */
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw4zP-qroaAqCpLDS0nLKY4WxgaHZX2o6TURZ5FJh2IPRbVL8dQ3nsQ1MTukgIQjt7W/exec';
+
+// (Không có dấu } ở cuối)
