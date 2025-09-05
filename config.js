e8473db0c4c50a784d3e6df4e82d781b419263b7
nguyenthach-2104 <mkt.mgbinhduong@gmail.com>
2025-09-05 15:41:55 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 // =================================================================
-// == FILE CẤU HÌNH CHO TRANG ĐĂNG NHẬP (CONFIG.JS) ==
+// == FILE CẤU HÌNH TRUNG TÂM (CONFIG.JS) ==
 // =================================================================
 
 /**
- * URL của Google Apps Script CHỈ dành cho việc xử lý các yêu cầu
- * liên quan đến xác thực người dùng như đăng nhập, quên mật khẩu, v.v.
+ * URL Apps Script cho chức năng Đăng nhập & Quản lý người dùng.
  */
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyt_qsGTqFojxWZB12Ut0O3RKMwxOpx_zIVg-aOB6AjIFaB6plwaxQ1dYtHveNGLjvxLQ/exec';
 
-// (Không có dấu } ở cuối)
+/**
+ * URL Apps Script cho chức năng Quản lý Khách hàng Tiềm năng (CRM).
+ */
+const CRM_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzLIZQioNcXs_EfYPHzpNaloPrxAqRIjnDMlzBHx4UMuD8Mc_Mf4LVDnky_wLTYD0A/exec';
